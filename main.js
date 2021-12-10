e634d021f1055ee0df8c84a4919600024539f46b
Ľubomír Druga <drugalubomir@gmail.com>
2021-12-10 11:16:17 +0100

    centered content

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 function mainText(text) {
     const element = document.getElementById("main-text");
     element.innerText = `${text}`;
-}
-
-function additionalText(text) {
-    const element = document.getElementById("additional-text");
-    element.innerText = `${text}`;
+    console.log(text);
 }
 
 function init() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     mainText(urlSearchParams.get('h1'));
-    additionalText(urlSearchParams.get('h2'));
 }
 
 init();
